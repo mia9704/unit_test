3e0165efaa065c532604a58d3f74d283d7ccd4d5
mia9704 <mmunkhja@ucsd.edu>
2016-04-19 15:34:00 -0700

    m

---
+++ app/src/androidTest/java/com/tests/JUnit_test.java
@@ -0,0 +1,25 @@
+package com.tests;
+import android.test.ActivityInstrumentationTestCase2;
+import com.example.user.lab3.MainActivity;
+import com.example.user.lab3.R;
+import android.widget.TextView;
+
+
+/**
+ * Created by user on 4/18/16.
+ */
+
+public class JUnit_test extends ActivityInstrumentationTestCase2<MainActivity>{
+    MainActivity mainActivity;
+    public JUnit_test() {
+        super(MainActivity.class);
+    }
+    public void test_first()
+    {
+        mainActivity = getActivity();
+        TextView textView = (TextView) mainActivity.findViewById(R.id.tv1);
+        String tester = textView.getText().toString();
+
+        assertEquals("Hello World!", tester);
+    }
+}
